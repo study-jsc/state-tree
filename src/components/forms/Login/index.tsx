aed2610442ace1c0c97ec54d416c6d2e6026fded
vuongnb <vuongnb@matdaibang.vn>
2020-11-19 13:57:41 +0700

    chore: update form

--- src/components/forms/Login/index.tsx
+++ src/components/forms/Login/index.tsx
@@ -1,6 +1,7 @@
 import { makeStyles, Typography } from "@material-ui/core";
 import { Button, Input } from "components/elements";
 import { useFormik } from "formik";
+import { useCallback } from "react";
 
 interface IProps {}
 
@@ -12,7 +13,7 @@ const useStyles = makeStyles((theme) => ({
 
 const LoginForm = () => {
   const classes = useStyles();
-  const { values, handleChange, handleSubmit } = useFormik({
+  const { values, handleSubmit, setFieldValue } = useFormik({
     initialValues: {
       username: "",
       password: "",
@@ -21,6 +22,13 @@ const LoginForm = () => {
       console.log({ values });
     },
   });
+
+  const setUsername = useCallback((value: string) => {
+    setFieldValue("username", value);
+  }, []);
+  const setPassword = useCallback((value: string) => {
+    setFieldValue("password", value);
+  }, []);
 
   return (
     <form>
@@ -34,7 +42,7 @@ const LoginForm = () => {
         variant="outlined"
         className={classes.mt2}
         value={values.username}
-        onChange={handleChange}
+        onChange={setUsername}
       />
       <Input
         fullWidth
@@ -44,7 +52,7 @@ const LoginForm = () => {
         type="password"
         className={classes.mt2}
         value={values.password}
-        onChange={handleChange}
+        onChange={setPassword}
       />
       <Button
         fullWidth
